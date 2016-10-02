91865c963e2819b6cfa3ab5149bfcfe3c49af81d
Trezy <tre@trezy.com>
2016-10-01 22:06:32 -0500

    Remove rescue gathering from rat model

--- js/collections/Rats.js
+++ js/collections/Rats.js
@@ -22,32 +22,6 @@ export default class Rats extends APICollection {
   }
 
   parseRecords (response) {
-//    let allRats = Backbone.Radio.channel('application').request('rats')
-//
-//    response.data.forEach(user => {
-//      let userRats = user.CMDRs
-//
-//      user.CMDRs = new RatsCollection
-//
-//      userRats.forEach(id => {
-//        let idHash = {
-//          id: id
-//        }
-//
-//        let ratModel = allRats.findWhere(idHash)
-//
-//        if (!ratModel) {
-//          ratModel = allRats.add(idHash)
-//        }
-//
-//        user.CMDRs.add(ratModel)
-//      })
-//    })
-//
-//    allRats.fetch({
-//      bulk: true
-//    })
-
     return response.data
   }
 
