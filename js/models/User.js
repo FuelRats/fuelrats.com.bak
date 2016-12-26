13e5f461cad65e67a44ff675bdd169098d22debd
Trezy <tre@trezy.com>
2016-12-25 22:41:40 -0600

    [WEB-34] Update User model to properly update nicknames before saving
    Previously, the User model would try to save the whole nicknames collection as
    an array of objects. The API doesn't like that -- rightfully so -- so we're
    making sure the collection gets converted back to an array of strings before
    saving.

--- js/models/User.js
+++ js/models/User.js
@@ -1,3 +1,4 @@
+import Backbone from 'backbone'
 import cookie from 'cookies-js'
 
 import BaseModel from './Base'
@@ -98,7 +99,9 @@ export default class User extends BaseModel {
   \******************************************************************************/
 
   addNickname (nickname) {
-    this.set('nicknames', this.get('nicknames').concat(nickname))
+    this.get('nicknames').add({
+      nickname: nickname
+    })
   }
 
   addRat (CMDRname) {
@@ -301,25 +304,29 @@ export default class User extends BaseModel {
 
   removeNickname (nickname) {
     let nicknames = this.get('nicknames')
-    let index = nicknames.indexOf(nickname)
+    let nicknameModel = nicknames.findWhere({
+      nickname: nickname
+    })
 
-    if (index !== -1) {
-      nicknames.splice(index, 1)
-    }
-
-    this.set('nicknames', nicknames)
+    nicknames.remove(nicknameModel)
   }
 
   toJSON (options) {
     let clone = super.toJSON(options)
 
-    clone.nicknames.map(nicknameModel => {
-      return nicknameModel.nickname
-    })
+    if (clone.nicknames instanceof Backbone.Collection) {
+      clone.nicknames = clone.nicknames.map(model => {
+        return model.get('nickname')
+      })
+    }
 
     delete clone.password
 
     return clone
+  }
+
+  toViewJSON (options) {
+    return super.toJSON(options)
   }
 
 
